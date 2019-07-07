d1242521780a82c551e5c58a2a0e242dfaa840e3
shibayan <me@shibayan.jp>
2019-07-07 18:47:23 +0900

    Adding ASP.NET Core sample code

---
+++ samples/MvcApp/Controllers/BuchizoController.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class BuchizoController : Controller
+    {
+        public BuchizoController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = _httpClientFactory.CreateClient("Buchizo");
+
+            var response = await httpClient.GetStringAsync("/");
+
+            return Content(response);
+        }
+    }
+}
