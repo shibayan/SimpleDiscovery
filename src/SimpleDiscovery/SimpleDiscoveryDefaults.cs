d0c5d95a772ecc7c28012b569b51779acfb43e02
shibayan <me@shibayan.jp>
2019-07-10 14:12:52 +0900

    Minor refactoring

---
+++ src/SimpleDiscovery/SimpleDiscoveryDefaults.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleDiscovery
+{
+    public static class SimpleDiscoveryDefaults
+    {
+        public const string DefaultKeyPrefix = "Registry";
+        public const string DefaultTagName = "Registry";
+    }
+}
